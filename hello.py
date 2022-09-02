78133b104d67ef6461a982da0e562f7cc434aa60
Stavros Kounis <skounis@gmail.com>
2022-09-02 12:07:12 +0000

    chore(code) Support python 3.5

--- hello.py
+++ hello.py
@@ -3,4 +3,7 @@ def add(x, y):
 
 #var=
 result = add(1, 2)
-print(f"This is the sum: 1, 2, {result}")
+# Python > 3.5
+# print(f"This is the sum: 1, 2, {result}")
+# Python 3.5
+print("This is the sum: 1, 2, %s" % result)
